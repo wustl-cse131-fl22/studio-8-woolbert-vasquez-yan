4f224529423606fe4eb1e8163da6109084cb7d6a
andrewwoolbert <andrewwoolbert@andrews-mbp.dhcp.wustl.edu>
2022-11-17 13:29:07 -0600

    Done

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -1,10 +1,73 @@
 package studio8;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Objects;
+
 public class Date {
 
+	private int month;
+	private int day;
+	private int year;
+	private boolean holiday;
+	
+	/**
+	 * 
+	 * @param month: The month on that date
+	 * @param day: The day of that date
+	 * @param year: The year of that date
+	 * @param holiday: If the date is a holiday or not
+	 */
+	
+	public Date(int month, int day, int year, boolean holiday) {
+		this.month = month;
+		this.day = day;
+		this.year = year;
+		this.holiday = holiday;
+	}
+	
+	public String toString() {
+		if (holiday) {
+			return month + "/" + day + "/" + year + ": Is a holiday!";
+		} else {
+			return month + "/" + day + "/" + year;
+		}
+	}
 
-    public static void main(String[] args) {
+	@Override
+	public int hashCode() {
+		return Objects.hash(day, month, year);
+	}
 
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Date other = (Date) obj;
+		return day == other.day && month == other.month && year == other.year;
+	}
+	
+	public static void main(String[] args) {
+		   Date date1 = new Date(1, 1, 2000, true);
+		   Date date2 = new Date(1, 1, 2001, false);
+		   Date date3 = new Date(1, 1, 2002, true);
+		   Date date4 = new Date(1, 1, 2003, false);
+		   Date date5 = new Date(1, 1, 2004, true);
+		   Date date6 = new Date(1, 1, 2005, false);
+		   
+		   LinkedList<Date> list = new LinkedList<Date>();
+		   HashSet<Date> set = new HashSet<Date>();
+		   
+		   list.add(date1);
+		   list.add(date1);
+		   set.add(date1);
+		   set.add(date1);
+		   
+		   System.out.println(set);
     }
 
 }
--- src/studio8/Time.java
+++ src/studio8/Time.java
@@ -1,11 +1,58 @@
 package studio8;
 
+import java.util.Objects;
 
 public class Time {
 
+	private int hour;
+	private int minute;
+	private boolean twelveHour;
+	
+	/**
+	 * 
+	 * @param hour: The hour at a certain time
+	 * @param minute: The minute at a certain time
+	 * @param twelveHour: If the time should be displayed in a twelve hour format
+	 */
+	
+	public Time(int hour, int minute, boolean twelveHour) {
+		this.hour = hour;
+		this.minute = minute;
+		this.twelveHour = twelveHour;
+	}
+	
+	public String toString() {
+		if (twelveHour) {
+			if (hour >= 12) {
+				return hour%12 + ":" + minute + " pm";
+			} else {
+				return hour%12 + ":" + minute + " am";
+			}
+			
+		} else {
+			return hour + ":" + minute;
+		}
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(hour, minute);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Time other = (Time) obj;
+		return hour == other.hour && minute == other.minute;
+	}
+
 	public static void main(String[] args) {
- 
-    	
+  
     }
 
 }
